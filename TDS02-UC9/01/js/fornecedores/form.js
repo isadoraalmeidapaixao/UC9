3ccdf56a1fb6709e1bd662295852ef1f32674888
isadora <isadoraalmeidapaixao@gmail.com>
2026-06-02 19:50:24 -0300

    adição de página de cadastro de fornecedor

---
+++ TDS02-UC9/01/js/fornecedores/form.js
@@ -0,0 +1,46 @@
+const form = document.getElementById(form-fornecedor);
+
+async function salvarFornecedor() {
+
+    form.addEventListener('submit', async(e) => {
+        e.preventDefault();
+
+        const nome = document.getElementById('nomeFantasia').value
+        const cnpj = parseFloat(document.getElementById('cnpj').value)
+
+        if (!nome || !cnpj) {
+            alert('Por favor, preencha todos os campos obrigatórios!');
+            return;
+        }
+
+        if(quantidade < 0) {
+            alert('A quantidade não pode ser menor que 0!');
+            return;
+        }
+        
+        const fornecedorDados = {
+            nome: nome,
+            cnpj: cnpj,
+        }
+
+        const url = `${API_BASE_URL}/fornecedores`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(fornecedorDados)
+            });
+
+            if (!response.ok) throw new Error("Erro ao salvar fornecedor")
+
+            window.location.href = 'index.html';
+        } catch (error) {
+            console.error("Erro ao salvar: ", error);
+            alert('Erro ao salvar o fornecedor. Tente novamente!')
+        }
+
+    })
+}
+
+salvarProduto();
